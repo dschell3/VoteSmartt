13210e2dcb13dcd6e88422800b4dc8070daa6fda
Darren Schell <dschell9876@gmail.com>
2025-11-26 12:49:54 -0800

    cleaning up codebase + making more readable

--- check_option_table.py
+++
@@ -1,7 +0,0 @@
-from flask_app.config.mysqlconnection import connectToMySQL  # Maintenance note: Minimal existence probe; superseded by diagnostics/inspect_schema; safe to delete.
-
-try:
-    res = connectToMySQL().query_db("SHOW TABLES LIKE 'option';")
-    print(res)
-except Exception as e:
-    print('ERROR:', e)
